6ed44b98f2f5314f168707381e45417f00d59523
Ian-Ma <spamforian@outlook.com>
2015-01-22 12:47:48 -0500

    Completed Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve calculate the inverse of a matrix. If the 
+## inverse of the matrix has aleady been calculated then cacheSolve will return 
+## the already computed inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of several elements. 
 
-makeCacheMatrix <- function(x = matrix()) {
-  
+makeCacheMatrix <-function(x){
+  mat <- NULL
+  set <- function(y) {
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) mat <<- solve
+  getinv <- function() mat
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve checks to see if an inverse matrix has already been calculated. 
+## If it has then it returns the already calculated value. If it hasn't it 
+## calculates the inverse of the matrix stored in the makeCacheMatrix function 
+## and then prints and stores this value in the list. 
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+ 
+  mat <- x$getinv()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setinv(mat)
+  mat
 }
+
+
--- makevector.R
+++ makevector.R
@@ -1,14 +1,13 @@
-makeVector <- function (x = numeric ()) {
+makeVector <- function(x = numeric()) {
   m <- NULL
-  set <- function (y) {
-      x <<- y
-      m <<- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
   }
-  get <- function () x
+  get <- function() x
   setmean <- function(mean) m <<- mean
-  getmean <- function () m
-  list (set = set, get = get, 
-        setmean = setmean,
-        getmean = getmean)
-
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
